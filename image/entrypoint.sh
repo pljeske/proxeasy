edc1170ac91cd36ca31739105d13f3875a0a0c57
Peer-Lucas Jeske <lucas.jeske@posteo.de>
2023-04-07 15:34:20 +0200

    add http only support

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -4,16 +4,23 @@ if [ ! -f "/app/config/config.yaml" ]; then
   echo "Please provide yaml config file in /app/config/config.yaml"
   exit 1;
 fi
-if [ ! -f "/app/config/dhparam.pem" ]; then
-  echo "Generating dhparams bc they don't exist in the /app/config directory..."
-  openssl dhparam -out /app/config/dhparam.pem 4096
+
+if [ "$HTTP_ONLY" == "false" ]; then
+  if [ ! -f "/app/config/dhparam.pem" ]; then
+    echo "Generating dhparams bc they don't exist in the /app/config directory..."
+    openssl dhparam -out /app/config/dhparam.pem 4096
+  fi
+  cp /app/config/dhparam.pem /etc/nginx/dhparam.pem
 fi
-cp /app/config/dhparam.pem /etc/nginx/dhparam.pem
 
-
+echo "Templating proxy nginx config..."
 #find /app/config -type f -name "*.json"  -exec sh -c 'filename="$1"; j2 /app/tmpl/proxy.j2 "/app/config/$filename" > /etc/nginx/sites-enabled/"${filename%.json}.conf"' shell {} \;
 #cat /app/config/config.yaml | j2 --format=yaml /app/proxy.j2 > /etc/nginx/proxies.conf
-j2 /app/proxy.j2 /app/config/config.yaml > /etc/nginx/proxies.conf
+if [ "$HTTP_ONLY" == "false" ]; then
+  j2 /app/proxy.j2 /app/config/config.yaml > /etc/nginx/proxies.conf
+else
+  j2 /app/http-only-proxy.j2 /app/config/config.yaml > /etc/nginx/proxies.conf
+fi
 
 echo "Starting nginx...";
 nginx -g "daemon off;"
